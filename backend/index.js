05de9ee261ae349c244f8fd5c82ad60aac80c5b2
Satyam Raj <355rajsatyam@gmail.com>
2025-08-15 00:56:25 +0530

    Updated functionality

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import userRoutes from './routes/user.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { serve } from "inngest/express";
+import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-created.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -13,6 +18,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets ", ticketRoutes);
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
